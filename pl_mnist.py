b40497f5e80526de3f9f2188315d05faecd03a60
Luca Antiga <luca.antiga@orobix.com>
2021-02-18 15:03:56 +0100

    Add export ONNX and TorchScript

--- pl_mnist.py
+++ pl_mnist.py
@@ -44,6 +44,8 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--max_epochs', type=int, default=10)
     parser.add_argument('--data_dir', type=str, default=os.getcwd())
+    parser.add_argument('--torchscript_file', type=str, default=None)
+    parser.add_argument('--onnx_file', type=str, default=None)
     args = parser.parse_args()
 
     dataset = MNIST(args.data_dir, download=True, transform=transforms.ToTensor())
@@ -55,3 +57,11 @@ if __name__ == '__main__':
     # most basic trainer, uses good defaults (auto-tensorboard, checkpoints, logs, and more)
     trainer = pl.Trainer(gpus=args.gpus, max_epochs=args.max_epochs)
     trainer.fit(model, train_loader)
+
+    if args.torchscript_file:
+        scripted_model = model.to_torchscript()
+        torch.jit.save(scripted_model, args.torchscript_file)
+
+    if args.onnx_file:
+        input_sample, _ = dataset[0]
+        model.to_onnx(args.onnx_file, input_sample, export_params=True)
