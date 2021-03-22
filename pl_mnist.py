701d9dc9c4f38f34c899e2f3abd187ab0ab18ee2
Rick Izzo <rlizzo@users.noreply.github.com>
2021-03-22 14:12:51 -0400

    Update pl_mnist.py

--- pl_mnist.py
+++ pl_mnist.py
@@ -1,3 +1,9 @@
+from six.moves import urllib
+
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+urllib.request.install_opener(opener)
+
 import os
 import torch
 import torch.nn.functional as F
